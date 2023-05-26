d690b4718b9ce9dacd729d3e8c6b6d4a92e1f1d9
Arnold Rispress <arnold.rispress@gmail.com>
2023-05-26 10:01:48 -0400

    added code to ask if the user wants to play again

--- Program.cs
+++ Program.cs
@@ -9,25 +9,29 @@ namespace Quest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name:");
-            string playerName = Console.ReadLine();
+            bool playAgain = true;
 
-            Adventurer theAdventurer = new Adventurer(playerName);
-            // Create a few challenges for our Adventurer's quest
-            // The "Challenge" Constructor takes three arguments
-            //   the text of the challenge
-            //   a correct answer
-            //   a number of awesome points to gain or lose depending on the success of the challenge
-            Challenge twoPlusTwo = new Challenge("2 + 2?", 4, 10);
-            Challenge theAnswer = new Challenge(
+            while(playAgain)
+            {
+                Console.WriteLine("Enter your name:");
+                string playerName = Console.ReadLine();
+
+                Adventurer theAdventurer = new Adventurer(playerName);
+                // Create a few challenges for our Adventurer's quest
+                // The "Challenge" Constructor takes three arguments
+                //   the text of the challenge
+                //   a correct answer
+                //   a number of awesome points to gain or lose depending on the success of the challenge
+                Challenge twoPlusTwo = new Challenge("2 + 2?", 4, 10);
+                Challenge theAnswer = new Challenge(
                 "What's the answer to life, the universe and everything?", 42, 25);
-            Challenge whatSecond = new Challenge(
+                Challenge whatSecond = new Challenge(
                 "What is the current second?", DateTime.Now.Second, 50);
 
-            int randomNumber = new Random().Next() % 10;
-            Challenge guessRandom = new Challenge("What number am I thinking of?", randomNumber, 25);
+                int randomNumber = new Random().Next() % 10;
+                Challenge guessRandom = new Challenge("What number am I thinking of?", randomNumber, 25);
 
-            Challenge favoriteBeatle = new Challenge(
+                Challenge favoriteBeatle = new Challenge(
                 @"Who's your favorite Beatle?
                 1) John
                 2) Paul
@@ -35,49 +39,54 @@ namespace Quest
                 4) Ringo
                 ",
                 4, 20
-            );
+                );
 
-            // "Awesomeness" is like our Adventurer's current "score"
-            // A higher Awesomeness is better
+                // "Awesomeness" is like our Adventurer's current "score"
+                // A higher Awesomeness is better
 
-            // Here we set some reasonable min and max values.
-            //  If an Adventurer has an Awesomeness greater than the max, they are truly awesome
-            //  If an Adventurer has an Awesomeness less than the min, they are terrible
-            int minAwesomeness = 0;
-            int maxAwesomeness = 100;
+                // Here we set some reasonable min and max values.
+                //  If an Adventurer has an Awesomeness greater than the max, they are truly awesome
+                //  If an Adventurer has an Awesomeness less than the min, they are terrible
+                int minAwesomeness = 0;
+                int maxAwesomeness = 100;
 
-            // A list of challenges for the Adventurer to complete
-            // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
-            List<Challenge> challenges = new List<Challenge>()
-            {
-                twoPlusTwo,
-                theAnswer,
-                whatSecond,
-                guessRandom,
-                favoriteBeatle
-            };
+                // A list of challenges for the Adventurer to complete
+                // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
+                List<Challenge> challenges = new List<Challenge>()
+                {
+                    twoPlusTwo,
+                    theAnswer,
+                    whatSecond,
+                    guessRandom,
+                    favoriteBeatle
+                };
 
-            // Loop through all the challenges and subject the Adventurer to them
-            //ASKS THE USER ONE QUESTION AT TIME AS OPPOSED TO ALL AT ONCE
-            foreach (Challenge challenge in challenges)
-            {
-                challenge.RunChallenge(theAdventurer);
-            }
+                // Loop through all the challenges and subject the Adventurer to them
+                //ASKS THE USER ONE QUESTION AT TIME AS OPPOSED TO ALL AT ONCE
+                foreach (Challenge challenge in challenges)
+                {
+                    challenge.RunChallenge(theAdventurer);
+                }
 
-            // This code examines how Awesome the Adventurer is after completing the challenges
-            // And praises or humiliates them accordingly
-            //OUTPUTS A PARTICULAR LINE AT THE END DEPENDING ON THE AWESOMENESS SCORE FROM ADVENTURER.CS
-            if (theAdventurer.Awesomeness >= maxAwesomeness)
-            {
-                Console.WriteLine("YOU DID IT! You are truly awesome!");
-            }
-            else if (theAdventurer.Awesomeness <= minAwesomeness)
-            {
-                Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
-            }
-            else
-            {
-                Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                // This code examines how Awesome the Adventurer is after completing the challenges
+                // And praises or humiliates them accordingly
+                //OUTPUTS A PARTICULAR LINE AT THE END DEPENDING ON THE AWESOMENESS SCORE FROM ADVENTURER.CS
+                if (theAdventurer.Awesomeness >= maxAwesomeness)
+                {
+                 Console.WriteLine("YOU DID IT! You are truly awesome!");
+                }
+                else if (theAdventurer.Awesomeness <= minAwesomeness)
+                {
+                    Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+                }
+                else
+                {
+                    Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                }
+
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                string playAgainInput = Console.ReadLine().ToLower();
+                playAgain = playAgainInput == "yes" || playAgainInput == "y";
             }
         }
     }
